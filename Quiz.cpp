59fe3b8f9e4a492c9dc39864fc237ca4481aeaa0
RoyJH <royjh@okstate.edu>
2018-01-31 18:24:02 -0600

    Quiz
    incomplete

---
+++ Quiz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int first = 1, second = 2;
+	bool repeat = true
+
+	cout << "What is the first you'd like to calculate?: \n"
+	cin >> first;
+
+	cout << "What is the second number you'd like to calculate?: \n"
+	cin >> second;
+
+	cout << "What operation should be performed? Add(+) or Multiply(*): "
+	cin <<
+
+	//INCOMPLETE 
+	return 0;
+}
