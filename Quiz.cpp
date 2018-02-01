e43d8709a654820162c99089d82808213ca7f273
RoyJH <royjh@okstate.edu>
2018-01-31 18:26:47 -0600

    Still not complete

--- Quiz.cpp
+++ Quiz.cpp
@@ -5,6 +5,7 @@ int main(){
 	int first = 1, second = 2;
 	bool repeat = true
 
+	while (repeat = true){
 	cout << "What is the first you'd like to calculate?: \n"
 	cin >> first;
 
@@ -13,7 +14,7 @@ int main(){
 
 	cout << "What operation should be performed? Add(+) or Multiply(*): "
 	cin <<
-
+	}
 	//INCOMPLETE 
 	return 0;
 }
