2622f0c33c183910935c32f15a04d0abfa6338f5
RoyJH <royjh@okstate.edu>
2018-01-31 18:58:57 -0600

    Working
    Class walkthrough

--- Quiz.cpp
+++ Quiz.cpp
@@ -2,19 +2,29 @@
 using namespace std;
 
 int main(){
-	int first = 1, second = 2;
-	bool repeat = true
+	int num1 = 0;
+	int num2 = 0;
+	bool next = true;
+	char action = 'c';
+	
+	while(next){
 
-	while (repeat = true){
-	cout << "What is the first you'd like to calculate?: \n"
-	cin >> first;
+		cout << "What is the first you'd like to calculate?: \n";
+		cin >> num1;
 
-	cout << "What is the second number you'd like to calculate?: \n"
-	cin >> second;
+		cout << "What is the second number you'd like to calculate?: \n";
+		cin >> num2;
 
-	cout << "What operation should be performed? Add(+) or Multiply(*): "
-	cin <<
+		cout << "Type + or * for what you want to do\n";
+		cin >> action;
+
+		if(action == '+'){
+			cout << "Your sum is " << num1 + num2 << endl;
+		}else if (action == '*'){
+			cout << "Your product is " << num1 * num2 << endl;
+		}
 	}
+	
 	//INCOMPLETE 
 	return 0;
 }
